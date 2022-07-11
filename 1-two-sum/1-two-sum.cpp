360b43d71396c0a8e9c668b96f1bf9102e138fcb
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2022-07-12 02:54:28 +0530

    Time: 13 ms (84.06%), Space: 11.1 MB (24.42%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,17 +1,33 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int t) {
-        unordered_map<int,int>mpp;
-        vector<int> v;
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int t) {
+//         unordered_map<int,int>mpp;
+//         vector<int> v;
+//         for(int i=0;i<nums.size();i++){
+//             if(mpp.find(t-nums[i])!=mpp.end()){
+//                 v.push_back(mpp[(t-nums[i])]);
+//                 v.push_back(i);
+//                 return v;
+//             }
+//             mpp[nums[i]]=i;
+//         }
+//         return v;
+        
+//     }
+// };
+class Solution{
+  public:
+    vector<int> twoSum(vector<int>&nums,int t){
+        map<int,int>mp;
+        vector<int>v;
         for(int i=0;i<nums.size();i++){
-            if(mpp.find(t-nums[i])!=mpp.end()){
-                v.push_back(mpp[(t-nums[i])]);
+            if(mp.find(t-nums[i])!=mp.end()){
+                v.push_back(mp[t-nums[i]]);
                 v.push_back(i);
                 return v;
             }
-            mpp[nums[i]]=i;
+            mp[nums[i]]=i;
         }
         return v;
-        
     }
 };
