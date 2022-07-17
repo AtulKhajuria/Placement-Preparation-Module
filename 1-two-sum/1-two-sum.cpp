458bf9cf9e4bf88c5b2f33dd5d7f0a63bb616152
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2022-07-17 12:34:09 +0530

    Update 1-two-sum.cpp

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int t) {
-//         unordered_map<int,int>mpp;
-//         vector<int> v;
-//         for(int i=0;i<nums.size();i++){
-//             if(mpp.find(t-nums[i])!=mpp.end()){
-//                 v.push_back(mpp[(t-nums[i])]);
-//                 v.push_back(i);
-//                 return v;
-//             }
-//             mpp[nums[i]]=i;
-//         }
-//         return v;
-        
-//     }
-// };
 class Solution{
   public:
     vector<int> twoSum(vector<int>&nums,int t){
