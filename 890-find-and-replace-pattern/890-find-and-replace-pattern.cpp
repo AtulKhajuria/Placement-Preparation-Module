b3cac5ab39c733a13a1f3b1a3500c89bbcd105b8
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2022-07-30 01:37:34 +0530

    Time: 10 ms (28.33%), Space: 8.6 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -25,39 +25,5 @@ public:
                 ans.push_back(words[i]);
         }
         return ans;
-        // vector<string>ans;
-        // vector<int>v;
-        // char prev=pattern[0];
-        // int k=0;
-        // v.push_back(1);
-        // for(int i=1;i<pattern.size();i++){
-        //     if(pattern[i]==prev)
-        //         v[k]++;
-        //     else{
-        //     v.push_back(1);
-        //     prev=pattern[i];
-        //     k++;
-        //     }
-        // }
-        // for(int i=0;i<words.size();i++){
-        //     vector<int>temp;
-        //     temp.clear();
-        //     temp.push_back(1);
-        //     int k1=0;
-        //     char tempPrev=words[i][0];
-        //     for(int j=1;j<words[i].size();j++){
-        //         if(words[i][j]==tempPrev)
-        //             temp[k1]++;
-        //         else{
-        //             temp.push_back(1);
-        //             tempPrev=words[i][j];
-        //             k1++;
-        //         }
-        //     }
-        //     if(temp==v)
-        //         ans.push_back(words[i]);
-        // }
-        // return ans;
-        
     }
 };
