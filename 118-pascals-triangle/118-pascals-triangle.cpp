bcee8c40cba764cccddb7a094b862541e0496d20
AtulKhajuria <84517651+AtulKhajuria@users.noreply.github.com>
2022-08-10 00:34:21 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -28,11 +28,26 @@
 //             return ans;
 //         }
 // };
+// class Solution{
+//     public:
+//     vector<vector<int>>generate(int numRows){
+//         vector<vector<int>>ans(numRows);
+//         for(int i=0;i<numRows;i++){
+//             ans[i].resize(i+1);
+//             ans[i][0]=ans[i][i]=1;
+//             for(int j=1;j<i;j++){
+//                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution{
     public:
     vector<vector<int>>generate(int numRows){
         vector<vector<int>>ans(numRows);
-        for(int i=0;i<numRows;i++){
+        ans[0].push_back(1);
+        for(int i=1;i<numRows;i++){
             ans[i].resize(i+1);
             ans[i][0]=ans[i][i]=1;
             for(int j=1;j<i;j++){
